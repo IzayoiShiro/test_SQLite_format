d9f3148e850384accea33632c52fe77a4fd223d0
unknown <>
2016-07-14 15:38:22 +0800

    Second commit

--- app/src/main/java/com/example/test_sqlite_format/MainActivity.java
+++ app/src/main/java/com/example/test_sqlite_format/MainActivity.java
@@ -8,10 +8,12 @@ import com.example.test_sqlite_format.database.MySQLiteOpenHelper;
 public class MainActivity extends AppCompatActivity {
 
     private MySQLiteOpenHelper mHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mHelper = new MySQLiteOpenHelper(this);
+        mHelper.getWritableDatabase();
     }
 }
--- app/src/main/java/com/example/test_sqlite_format/database/MySQLiteOpenHelper.java
+++ app/src/main/java/com/example/test_sqlite_format/database/MySQLiteOpenHelper.java
@@ -3,6 +3,8 @@ package com.example.test_sqlite_format.database;
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
+import android.text.LoginFilter;
+import android.util.Log;
 
 import com.example.test_sqlite_format.database.PersonTable.Person;
 
@@ -10,21 +12,25 @@ import com.example.test_sqlite_format.database.PersonTable.Person;
  * Created by 杨天宇 on 2016/7/14.
  */
 public class MySQLiteOpenHelper extends SQLiteOpenHelper {
+    private static final String TAG = "MySQLiteOpenHelper";
     public MySQLiteOpenHelper(Context context) {
         super(context, "person", null, 1);
     }
 
     @Override
     public void onCreate(SQLiteDatabase sqLiteDatabase) {
-        sqLiteDatabase.execSQL("create table "+ Person.TABLE_NAME+"("+
-                Person._ID + " integer primary key increment,"+
-                Person.NAME + " varchar(20),"+
+        sqLiteDatabase.execSQL("create table " + Person.TABLE_NAME + "(" +
+                Person._ID + " integer primary key autoincrement," +
+                Person.NAME + " varchar(20)," +
                 Person.NUMBER + " varchar(20))"
         );
+        Log.i(TAG, "onCreate: 数据库被创建");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
-
+        sqLiteDatabase.execSQL("alter table "
+                + Person.TABLE_NAME + " add account varchar(20)"
+        );
     }
 }
